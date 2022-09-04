f71be340eede490c899dfa9d3b8414a1797770e7
Ari-chan <arielle.elak@gmail.com>
2022-09-04 15:48:22 -0600

    Initial mock HTML layout

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+/*
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ * Weather Dashboard JS
+ * Last Edited by Arielle Sept 4 2022
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+*/
+
+var weatherAPI = "435fcbf53b60753e425fe48c7b6d8c16";
