ea6a2ff6597b03575ed79c8a804b034af03110b6
Ari-chan <arielle.elak@gmail.com>
2022-09-14 21:59:53 -0600

    Function checks on load if existing local storage, redirects to dashboard if previous user

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,10 +5,16 @@
  * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 */
 
-
+/**
+ * On page load, detects whether user has been here before
+ * (Has existing local storage data)
+ * If so, immediately redirects them to the dashboard with their last searched city
+ */
 function checkStorage() {
     if (localStorage.getItem('previousCities') !== null) {
-        console.log("Welcome back!")
+        console.log("Welcome back!");
+        window.location.replace("/dashboard.html");
     } else {
-    }
+        console.log("Welcome, new user!");
+    };
 };
